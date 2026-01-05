8a3d84ff7fc9ea97470f6ac0be9ea7b00417c38e
beel <crazybeel97@gmail.com>
2026-01-04 19:53:53 -0600

    fix: multimonitor best practice

--- river/conf.d/06-keybindings-multimonitor.sh
+++ river/conf.d/06-keybindings-multimonitor.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # --- Control Multi-Monitor ---
-riverctl map normal $mod BracketLeft focus-output previous
-riverctl map normal $mod BracketRight focus-output next
-riverctl map normal $mod+Shift BracketLeft spawn "~/.config/river/scripts/send-to-output.sh previous"
-riverctl map normal $mod+Shift BracketRight spawn "~/.config/river/scripts/send-to-output.sh next"
+riverctl map normal $mod BracketLeft focus-output left
+riverctl map normal $mod BracketRight focus-output right
+riverctl map normal $mod+Shift BracketLeft spawn "~/.config/river/scripts/send-to-output.sh left"
+riverctl map normal $mod+Shift BracketRight spawn "~/.config/river/scripts/send-to-output.sh right"
--- river/conf.d/10-personal-scripts.sh
+++ river/conf.d/10-personal-scripts.sh
@@ -2,11 +2,16 @@
 
 common_scripts="$HOME/common_scripts"
 launchers="$common_scripts/launchers"
+system="$common_scripts/system"
 
 # --- Lanzadores de Aplicaciones ---
 riverctl map normal $mod A spawn "$launchers/w_app_launcher.sh"
 riverctl map normal $mod B spawn "$launchers/w_browser_launcher.sh"
+
 #riverctl map normal $mod+Shift W spawn "$launchers/dwm_wallpaper.sh"
+ 
+# --- wifi
+riverctl map normal $mod W spawn "$system/w_wifi_connect.sh"
 riverctl map normal $mod BackSpace spawn "$launchers/w_logout_options.sh"
 
 
