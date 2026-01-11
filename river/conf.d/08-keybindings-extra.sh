bc872808ccf8d4a3855c792760354261a31d00e6
beel <crazybeel97@gmail.com>
2026-01-11 16:36:12 -0600

    feat: integration with matugen wallapaper setter

--- river/conf.d/08-keybindings-extra.sh
+++ river/conf.d/08-keybindings-extra.sh
@@ -7,3 +7,6 @@ riverctl map normal $mod+Shift P spawn 'grim - | wl-copy'
 # --- UI y Elementos Visuales ---
 riverctl map normal Control+Mod1 W spawn "pkill -SIGUSR1 waybar"
 riverctl map normal $mod N spawn "swaync-client -t -sw"
+
+# -- wallpaper --
+riverctl map normal $mod+Shift W spawn "alacritty --class float-alacritty -e 'wallpaper_setter'"
