66888faa68719120a9cf78e8aa17b31cc705df89
beel <crazybeel97@gmail.com>
2026-04-02 22:08:12 -0600

    fix:colors

--- river/conf.d/00-variables.sh
+++ river/conf.d/00-variables.sh
@@ -5,10 +5,10 @@ mod="Mod4"
 term="alacritty"
 file_explorer="$term -e spf"
 
-# Carga de colores y configuraciones iniciales
-# This script is expected to be in the parent directory
-if [ -f "$(dirname "$0")/../matugen_colors_river.sh" ]; then
-    . "$(dirname "$0")/../matugen_colors_river.sh"
+# Carga de colores generados por matugen
+if [ -f "$HOME/.config/river/matugen_colors_river.sh" ]; then
+    . "$HOME/.config/river/matugen_colors_river.sh"
 fi
 
 riverctl border-width 4
+
