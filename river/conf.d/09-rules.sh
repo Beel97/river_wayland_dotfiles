783e20f885ea1dfdd0bbc84e59484ec5f834d98c
beel <crazybeel97@gmail.com>
2026-04-05 16:41:53 -0600

    fix: posicionamiento de mi wofi

--- river/conf.d/09-rules.sh
+++ river/conf.d/09-rules.sh
@@ -14,6 +14,9 @@ riverctl rule-add -title "Picture-in-Picture" float
 riverctl rule-add -title "Library" float # Firefox/Librewolf
 riverctl rule-add -app-id "firefox" -title "DevTools" float
 
+# Regla para que wofi flote y no tenga bordes si se usa --normal-window
+riverctl rule-add -app-id "wofi" float
+
 # Nota: En River, las ventanas FULLSCREEN tapan a las ventanas FLOTANTES.
 # Si quieres que la ventana principal de REAPER ocupe toda la pantalla
 # sin tapar los diálogos, usa el comando "zoom" (Mod + Enter) en lugar de 
